ccfc6f903380e50a56529178f5ff8e710a9b342a
KING6CODE <alistpassecoutrin@gmail.com>
2025-08-12 23:34:00 +0200

    Create DetailedMill.js

---
+++ components/DetailedMill.js
@@ -0,0 +1,9 @@
+// components/DetailedMill.js
+export default function DetailedMill() {
+  return (
+    <div>
+      {/* Placeholder pour DetailedMill */}
+      <h2>DetailedMill component</h2>
+    </div>
+  );
+}
